f0002355d0a556ef2b4692690333fdaf1d65825c
Csedem0 <emmasobula@gmail.com>
2024-05-05 00:47:12 +0100

    url removed blacklist import

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import CustomUserCreate, BlacklistTokenUpdateView
+from .views import CustomUserCreate
 
 app_name = 'users'
 
 urlpatterns = [
-    path('create/', CustomUserCreate.as_view(), name="create_user"),
+    path('create/', CustomUserCreate.as_view(), name='create_user'),
 ]
-
